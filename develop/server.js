6886818159a9491ba6148e5e52366a6d79f2c759
nbmc1991 <nbmc1991@gmail.com>
2020-11-28 18:40:25 -0500

    linked indexjs into notes.html

--- develop/server.js
+++ develop/server.js
@@ -2,27 +2,45 @@
 // Series of npm packages that we will use to give our server useful functionality
 
 const express = require("express");
-
-
-//tells node we are creating a n 'express' server
+const fs = require("fs");
+// const { allowedNodeEnvironmentFlags } = require("process");
+var database = require("../develop/db/db.json");  //getting db file to make changes in the notes
+//tells node app is express app
 var app = express();
-
-
-// const fs =require("fs");
-// const view = require("./routes/htmlRoutes");
 // Sets an initial port. Will use later in listener 
 var PORT = process.env.PORT || 8080;
 
-// FIND CORRECT MEANING
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); //using express.static and app.use to pull all the files from the public folder to use mostly from the css styling. 
+app.use(express.urlencoded({ extended: true }));   //using the app to express acess to the body requirements (req) function 
 app.use(express.json());
 
-// ROUTER
-//will map server to different possible routes ...different pages within
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+
+app.get("public/assets/js/index.js"), function (req, res){
+    res.sendFile(path.join(__dirname, "public/assets/js/index.js"))
+};     //express reaching out to index.js to send file after respond
+
+app.get("/", function(req, res){
+
+    res.sendFile(path.join(__dirname, "public/index.html"))
+ }); //ROUTES interact with public index html database:
+
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "/index.html"))
+});
+
+app.get("/api/notes", function(req, res){
+    return res.json();
+});  //API ROUTES interact with notes api db
+
+
 
 //LISTENER STARTS COMMUCATION/SERVER
 app.listen(PORT, function (){
     console.log( "App is listening on PORT:" + PORT);
+});
+
+
+app.post("/api/notes", function (req, res){
+
 });
