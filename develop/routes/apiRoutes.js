5eae8eeb7fd05caad21a8d1a9acdf729f465c992
nbmc1991 <nbmc1991@gmail.com>
2020-11-28 18:42:33 -0500

    moved empty saved note array into file and export so other files can use with require

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,25 +1,40 @@
-//linking our empty array that holds saved notes
-var savedNotes = require("../db/Notes");
+//creating an empty array to hold saved notes
+
+var savedNotesArray =[
+    {
+        noteTitle:"Today was a Good Day",
+        writtenNote:"bla bla bla"
+    },
+];
+
+//export the file to make it accessible for other files to use using require
+module.exports= savedNotesArray ;
+
+require("./htmlRoutes")(app);
+require("./apiRoutes")(app);
 
 
-module.exports = function (app){
+
+
+
+// module.exports = function (app){
     
-    app.get("/api/notes", function(req,res){
-        res.json(savedNotes);
-    });
+//     app.get("/api/notes", function(req,res){
+//         res.json(savedNotes);
+//     });
     
 
-    //POST
-    app.post("/api/notes", function (req, res){
-        // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-        if (savedNotes.length <5){
-            savedNotes.push(req.body);
-            res.json(true);
-        }
-        else{
-            return "Please, try again later." +false;
-        }
+//     //POST
+//     app.post("/api/notes", function (req, res){
+//         // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+//         if (savedNotes.length <5){
+//             savedNotes.push(req.body);
+//             res.json(true);
+//         }
+//         else{
+//             return "Please, try again later." +false;
+//         }
     
-    });
+//     });
     
-};
+// };
