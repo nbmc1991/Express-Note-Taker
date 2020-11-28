bbb3ebf379d27ab9b61be1db65d8e6b29df885f1
nbmc1991 <nbmc1991@gmail.com>
2020-11-28 18:43:58 -0500

    re routes paths to map possible user selections and export htmls

--- develop/routes/htmlRoutes.js
+++ develop/routes/htmlRoutes.js
@@ -1,24 +1,25 @@
 // We need to include the path package to get the correct file path for our html
- 
-// const router = require("express").Router();
 var path = require("path");
 
 //ROUTING
 
 module.exports = function (app) {
-
+    //if no matching route is fine default to home
+    app.get("*", function (req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    
 app.get("/notes", function (req, res){
 res.sendFile(path.join(__dirname, "../public/notes.html"));
 
 });
 
-//if no matching route is fine default to home
-app.get("*", function (req, res){
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-
 };
+
+//ROUTE files give the server a "map" of how to respond when user visits or request from diff URLs
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
 
