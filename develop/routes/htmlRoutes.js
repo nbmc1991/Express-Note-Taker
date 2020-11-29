af26c7260b4a4a90a8e8a9bf4f96bb1f7ea7ffe8
nbmc1991 <nbmc1991@gmail.com>
2020-11-28 20:25:52 -0500

    export app

--- develop/routes/htmlRoutes.js
+++ develop/routes/htmlRoutes.js
@@ -1,9 +1,10 @@
 // We need to include the path package to get the correct file path for our html
 var path = require("path");
+const app = require("express");
 
 //ROUTING
 
-module.exports = function (app) {
+
     //if no matching route is fine default to home
     app.get("*", function (req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
@@ -14,7 +15,7 @@ res.sendFile(path.join(__dirname, "../public/notes.html"));
 
 });
 
-};
+module.exports = app ; 
 
 //ROUTE files give the server a "map" of how to respond when user visits or request from diff URLs
 
