ad1e8cc0a93b662ade770498984f7a0e83497bce
nbmc1991 <nbmc1991@gmail.com>
2020-11-28 20:19:43 -0500

    re arranged routes get routes and functionals

--- develop/server.js
+++ develop/server.js
@@ -1,36 +1,42 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
-
 const express = require("express");
 const fs = require("fs");
-// const { allowedNodeEnvironmentFlags } = require("process");
-var database = require("../develop/db/db.json");  //getting db file to make changes in the notes
-//tells node app is express app
-var app = express();
-// Sets an initial port. Will use later in listener 
-var PORT = process.env.PORT || 8080;
+var db = require("../develop/db/db.json");  //getting db file to make changes in the notes
+var { v4: uuidv4 } = require("uuid");    // id maker
+var app = express();   //tell node its an express app
+const notes = require("./db/Notes");
+var path =require("path");
+var PORT = process.env.PORT || 8080;    // Sets an initial port. Will use later in listener 
 
-app.use(express.static("public")); //using express.static and app.use to pull all the files from the public folder to use mostly from the css styling. 
+//middleware
+app.use(express.static(path.join(__dirname,"public"))); //using express.static and app.use to pull all the files from the public folder to use mostly from the css styling. 
 app.use(express.urlencoded({ extended: true }));   //using the app to express acess to the body requirements (req) function 
 app.use(express.json());
 
 
-app.get("public/assets/js/index.js"), function (req, res){
-    res.sendFile(path.join(__dirname, "public/assets/js/index.js"))
-};     //express reaching out to index.js to send file after respond
 
-app.get("/", function(req, res){
+//ROUTES
+// app.get("public/assets/js/index.js"), function (req, res){
+//     res.sendFile(path.join(__dirname, "public/assets/js/index.js"))
+// };     //express reaching out to index.js to send file after respond
 
-    res.sendFile(path.join(__dirname, "public/index.html"))
- }); //ROUTES interact with public index html database:
+// app.get("/", function(req, res){
+
+//     res.sendFile(path.join(__dirname, "public/index.html"))
+//  }); //ROUTES interact with public index html database:
 
 
 app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "/index.html"))
+    res.sendFile(path.join(__dirname, "/public/notes.html"), function (err){
+        if (err) throw err;
+    })
 });
 
+
+//returns all notes
 app.get("/api/notes", function(req, res){
-    return res.json();
+   res.json(db);
    
 });  //API ROUTES interact with notes api db
 
