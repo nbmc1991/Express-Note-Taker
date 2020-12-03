21a85d0fdeffdeb47d7f25a9cc916c8812d8162f
nbmc1991 <nbmc1991@gmail.com>
2020-12-02 21:41:54 -0500

    re routed get routes for html

--- develop/routes/htmlRoutes.js
+++ develop/routes/htmlRoutes.js
@@ -3,13 +3,11 @@ var path = require("path");
 const router = require("express").Router();
 
 //ROUTING
-
-
 //wild card ** if no matching route is fine default to home
 
 router.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "../public/notes.html")));
 
-router.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+//router.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
 
 
 module.exports = router;
