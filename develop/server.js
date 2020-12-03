49678fc3ec223a7758835171bad8043d08f72e15
nbmc1991 <nbmc1991@gmail.com>
2020-12-02 21:43:25 -0500

    linked api routes and html routes properly

--- develop/server.js
+++ develop/server.js
@@ -5,7 +5,7 @@ const apiRoutes = require("./routes/apiRoutes");
 var htmlRoutes = require("./routes/htmlRoutes");
 var app = express();  //tells node its an express app
 var path = require("path");
-var router =express.Router();
+
 
 //PORT
 var PORT = process.env.PORT || 8080;    // Sets an initial port. Will use later in listener 
@@ -14,14 +14,10 @@ var PORT = process.env.PORT || 8080;    // Sets an initial port. Will use later 
 //middleware
 app.use(express.static(path.join(__dirname +"public"))); //using express.static and app.use to pull all the files from the public folder to use mostly from the css styling. 
 app.use(express.urlencoded({ extended: true }));   //using the app to express acess to the body requirements (req) function 
-app.use(express.json());
+app.use(express.static("public"));
 
-
-app.use("/", htmlRoutes);
-
-
-
-
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 //LISTENER STARTS COMMUCATION/SERVER
 app.listen(PORT, function (){
