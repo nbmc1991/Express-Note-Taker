7c78395ba51262ca7c90630f31cae3746b47faa1
nbmc1991 <nbmc1991@gmail.com>
2020-12-02 21:41:02 -0500

    post is set

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,9 +1,9 @@
 const db = require("../db/db.json");   //path to file were notes are going to be saved
 const fs = require("fs");
 var path = require("path");
-var {v4: uuidv4} = require("uuid"); // mesage id maker
+var { v4: uuidv4 } = require("uuid"); // mesage id maker
 var express = require("express");
-var router =express.Router();
+var router = express.Router();
 
 //GET ROUTE
 //returns notes.html
@@ -19,18 +19,37 @@ router.get("/api/notes", (req, res) => {
 
 // POST route
 router.post("/api/notes", (req, res) => {
-    var note =req.body;
-    console.log(req.params.id);
+    var note = req.body;
+    console.log(req.body);
     let id = uuidv4();
-    note.id=id; 
+    note.id = id;
     db.push(note);
-    fs.writeFile(path.join(__dirname, "/db/db.json"),
-    JSON.stringify(db),
-    (err) => {
+    fs.writeFile("./db/db.json", JSON.stringify(db), function (err) {
         if (err) throw err;
-    });
-    res.json(db);
+
+        res.json(db);
+    })
 });
 
+router.delete("/api/notes/:id", (req, res) => {
+    console.log(req.params)
 
-module.exports = router ;
+    // Get the content of the DB file
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        let parsed = JSON.parse(data)
+        let removed = parsed.filter(note => note.id !== req.params.id)
+        console.log(removed)
+        fs.writeFile("./db/db.json", JSON.stringify(removed), function (err) {
+            if (err) throw err;
+            res.json(db)
+        })
+    })
+
+    // Find the one with the ID that matches the id we get from the params
+    // IF it si there delete it
+    // Res.json done? 
+})
+
+
+module.exports = router;
